fd2f280b3b058344d78298f64b7155f899ab2a8e
Junhui Zhang <junhuizhang.g@gmail.com>
2024-06-02 14:54:08 +1200

    static home page

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,42 @@ import "./styles.css"
 
 function App() {
 
-  return <label
-  >Do you like peas?
-    <input type="checkbox" name="peas" />
-  </label>
+  return (
+    <>
+      <form className="new-item-form">
+        <div className="form-row">
+          <label htmlFor="new-item">New Item</label>
+          <input type="text" id="new-item" />
+        </div>
+        <button className="btn">Add Item</button>
+      </form>
+      <h1 className="header">Todo List</h1>
+      <ul className="list">
+        <li>
+          <label>
+            <input type="checkbox" />
+            Item1
+          </label>
+          <button className="btn btn-danger">Remove</button>
+        </li>
+        <li>
+          <label>
+            <input type="checkbox" />
+            Item2
+          </label>
+          <button className="btn btn-danger">Remove</button>
+        </li>
+        <li>
+          <label>
+            <input type="checkbox" />
+            Item3
+          </label>
+          <button className="btn btn-danger">Remove</button>
+        </li>
+      </ul>
+
+    </>
+  )
 
 
 }
